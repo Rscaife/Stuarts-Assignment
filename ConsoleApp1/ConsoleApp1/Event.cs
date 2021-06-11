e077ad381e0716f6d4fcad9efd31caeccada99c8
Rscaife <85740993+Rscaife@users.noreply.github.com>
2021-06-11 12:03:06 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tournament
+{
+    class Event
+    {
+        string eventname;
+        List<string> types = new List<string>();
+        string type;
+
+        public Event()
+        {
+            types.Add("academic");
+            types.Add("sporting");
+        }
+
+        public void setType(String s)
+        {
+            if(type.Contains(s))
+            {
+                type = s;
+            } else
+            {
+                Console.WriteLine("this is not valid type");
+            }
+        }
+    }
+}
